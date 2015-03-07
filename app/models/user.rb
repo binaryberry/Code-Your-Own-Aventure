15f12d43742509495019bfe131b7dba53e60d06d
Tatiana Soukiassian <binaryberry@gmail.com>
2015-03-07 16:13:30 +0000

    created a couple of views

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+require 'pry'
+
+class User
+
+	attr_accessor :name
+
+	def initialize(name)
+		@name = name
+	end
+
+end
--- app/server.rb
+++ app/server.rb
@@ -1,14 +1,30 @@
 require 'sinatra'
 require 'sinatra/partial'
+require 'pry'
 
 require_relative 'models/user.rb'
 
 class CodeAdv < Sinatra::Application
 
-    set :public_folder, File.join(File.dirname(__FILE__), '..', 'public') 
+    set :public_folder, File.join(File.dirname(__FILE__), '..', 'public')
+
+	use Rack::MethodOverride
+
+    enable :sessions
+	set :session_secret, 'super secret' 
     
     get '/' do
+    	session[:name]="Ellie"
         erb :index
+    end
+
+    post '/first_week' do
+    	redirect to('/first_week')
+    end
+
+    get '/first_week' do
+    	@user = User.new(session[:name])
+    	erb :first_week
     end
 
 end
